4c6ce462e3ca9c88e6c08904a70469b59f6fb1a9
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-14 12:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ function palindrome(str){
 	const string= str.toLowerCase();
 	const len=str.length;
 	//loop through half of the string
-	for(let i=0; i<len/2; i++)
+	for(let i=0; i<len; i++)
 		{
 			//check if first and last strings are same
-			if(string[i] !==string[len-1-i])
+			if(string[i] !==string[len-1])
 			{
 				return false;
 			}
-		}return true;
+		}
+	return true;
 
 }
 module.exports = palindrome
